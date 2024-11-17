749dc2e5a7e58404888c2c8563d73f8257b9b231
dinyar-dev <dinyar.dev@gmail.com>
2024-11-17 09:59:38 -0500

    setup pages in beforeTest hook

--- src/test/java/m4n4ge/selenium_autotests/SignupTests.java
+++ src/test/java/m4n4ge/selenium_autotests/SignupTests.java
@@ -1,6 +1,7 @@
 package m4n4ge.selenium_autotests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import m4n4ge.selenium_autotests.pages.DashboardPage;
 import m4n4ge.selenium_autotests.pages.LandingPage;
@@ -8,22 +9,26 @@ import m4n4ge.selenium_autotests.pages.SignupPage;
 
 
 public class SignupTests extends BaseTest {
+	private LandingPage landingPage;
+    private SignupPage signupPage;
+    private DashboardPage dashboardPage;
+
+    @BeforeTest
+    public void setupPages() {
+        this.landingPage = new LandingPage(driver);
+        this.signupPage = new SignupPage(driver);
+        this.dashboardPage = new DashboardPage(driver);
+    }
 
     @Test
     public void testUserSignup() {
-        LandingPage landingPage = new LandingPage(driver);
-
         landingPage.goTo("http://localhost:5173");
         Assert.assertTrue(landingPage.isLoaded());
 
         landingPage.clickSignUpButton();
-
-        SignupPage signupPage = new SignupPage(driver);
         Assert.assertTrue(signupPage.isLoaded());
 
         signupPage.signup("John","Doe", "JD@test.com", "Testing123!", "Testing123!");
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
         Assert.assertTrue(dashboardPage.isLoaded());
         
         dashboardPage.logout();
