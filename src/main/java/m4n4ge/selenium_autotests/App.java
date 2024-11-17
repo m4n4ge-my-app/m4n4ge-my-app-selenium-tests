1e5ce11bcc3fae7015146415d4bc0e7e92e60439
dinyar-dev <dinyar.dev@gmail.com>
2024-11-17 07:21:19 -0500

    create maven project

---
+++ src/main/java/m4n4ge/selenium_autotests/App.java
@@ -0,0 +1,13 @@
+package m4n4ge.selenium_autotests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
